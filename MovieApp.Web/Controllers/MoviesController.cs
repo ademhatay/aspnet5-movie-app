66e1006adabed217d4a73e3a7277191de1d3b827
Adem Hatay <hatayadem5@gmail.com>
2024-01-28 22:56:10 +0300

    Model List

--- MovieApp.Web/Controllers/MoviesController.cs
+++ MovieApp.Web/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MovieApp.Web.Models;
+using System.Collections.Generic;
 
 namespace MovieApp.Web.Controllers
 {
@@ -27,7 +28,13 @@ namespace MovieApp.Web.Controllers
         // /movies/list
         public IActionResult List()
         {
-            return View("Movies");
+            var filmListesi = new List<Movie>()
+            {
+                new Movie() { Title = "The Godfather", Description = "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.", Director = "Francis Ford Coppola", Players = new string[] { "Marlon Brando", "Al Pacino", "James Caan" } },
+                new Movie() { Title = "The Shawshank Redemption", Description =  "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.", Players = new string[] { "Tim Robbins", "Morgan Freeman", "Bob Gunton" } },
+                new Movie() { Title = "The Dark Knight", Description = "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.", Director = "Christopher Nolan", Players = new string[] { "Christian Bale", "Heath Ledger", "Aaron Eckhart" } },
+            };
+            return View("Movies", filmListesi);
         }
 
         // /movies/details
