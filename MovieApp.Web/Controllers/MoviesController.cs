ca1e754461bcb5fa34520c4de1ad810baa672167
Adem Hatay <hatayadem5@gmail.com>
2024-01-28 23:11:58 +0300

    Static Files

--- MovieApp.Web/Controllers/MoviesController.cs
+++ MovieApp.Web/Controllers/MoviesController.cs
@@ -30,9 +30,9 @@ namespace MovieApp.Web.Controllers
         {
             var filmListesi = new List<Movie>()
             {
-                new Movie() { Title = "The Godfather", Description = "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.", Director = "Francis Ford Coppola", Players = new string[] { "Marlon Brando", "Al Pacino", "James Caan" } },
-                new Movie() { Title = "The Shawshank Redemption", Description =  "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.", Players = new string[] { "Tim Robbins", "Morgan Freeman", "Bob Gunton" } },
-                new Movie() { Title = "The Dark Knight", Description = "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.", Director = "Christopher Nolan", Players = new string[] { "Christian Bale", "Heath Ledger", "Aaron Eckhart" } },
+                new Movie() { Title = "The Godfather", Description = "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.", Director = "Francis Ford Coppola", Players = new string[] { "Marlon Brando", "Al Pacino", "James Caan" }, ImageUrl = "Godfather.jpg" },
+                new Movie() { Title = "The Shawshank Redemption", Description =  "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.", Players = new string[] { "Tim Robbins", "Morgan Freeman", "Bob Gunton" }, ImageUrl = "The-Shawshank-Redemption.jpg"  },
+                new Movie() { Title = "The Dark Knight", Description = "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.", Director = "Christopher Nolan", Players = new string[] { "Christian Bale", "Heath Ledger", "Aaron Eckhart" }, ImageUrl = "The-Dark-Knight.jpg" },
             };
             return View("Movies", filmListesi);
         }
