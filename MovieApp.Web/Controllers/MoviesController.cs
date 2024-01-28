821288a45e051037f8602813f25a47fa347c9122
Adem Hatay <hatayadem5@gmail.com>
2024-01-29 02:56:52 +0300

    View Components

--- MovieApp.Web/Controllers/MoviesController.cs
+++ MovieApp.Web/Controllers/MoviesController.cs
@@ -12,23 +12,15 @@ namespace MovieApp.Web.Controllers
         {
             var filmListesi = new List<Movie>()
             {
-                new Movie() { Title = "The Godfather", Description = "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.", Director = "Francis Ford Coppola", Players = new string[] { "Marlon Brando", "Al Pacino", "James Caan" }, ImageUrl = "Godfather.jpg" },
-                new Movie() { Title = "The Shawshank Redemption", Description =  "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.", Players = new string[] { "Tim Robbins", "Morgan Freeman", "Bob Gunton" }, ImageUrl = "The-Shawshank-Redemption.jpg"  },
-                new Movie() { Title = "The Dark Knight", Description = "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.", Director = "Christopher Nolan", Players = new string[] { "Christian Bale", "Heath Ledger", "Aaron Eckhart" }, ImageUrl = "The-Dark-Knight.jpg" },
+                new Movie() { Title = "The Godfather", Description = "Description1", Players = new string[] { "Marlon Brando", "Al Pacino", "James Caan" }, ImageUrl = "Godfather.jpg" },
+                new Movie() { Title = "The Shawshank Redemption", Description =  "Description2", Players = new string[] { "Tim Robbins", "Morgan Freeman", "Bob Gunton" }, ImageUrl = "The-Shawshank-Redemption.jpg"  },
+                new Movie() { Title = "The Dark Knight", Description = "WDescription3", Director = "Christopher Nolan", Players = new string[] { "Christian Bale", "Heath Ledger", "Aaron Eckhart" }, ImageUrl = "The-Dark-Knight.jpg" },
             };
 
-            var turListesi = new List<Genre>()
-            {
-                new Genre() { Name = "Crime" },
-                new Genre() { Name = "Drama" },
-                new Genre() { Name = "Action" },
-                new Genre() { Name = "Thriller" }
-            };
-
+          
             var model = new MovieGenreViewModel()
             {
                 Movies = filmListesi,
-                Genres = turListesi
             };
 
             return View("Movies", model);
---
+++ MovieApp.Web/ViewComponents/GenresViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp.Web.Models;
+using System.Collections.Generic;
+
+namespace MovieApp.Web.ViewComponents
+{
+    public class GenresViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var turListesi = new List<Genre>()
+            {
+                new Genre() { Name = "Crime" },
+                new Genre() { Name = "Drama" },
+                new Genre() { Name = "Action" },
+                new Genre() { Name = "Thriller" }
+            };
+
+            return View(turListesi);
+        }
+    }
+}
