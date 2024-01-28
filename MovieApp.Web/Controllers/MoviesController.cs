5e39c266f736a1a383319964f61c71937b43a5a8
Adem Hatay <hatayadem5@gmail.com>
2024-01-29 02:37:40 +0300

    View Models

--- MovieApp.Web/Controllers/MoviesController.cs
+++ MovieApp.Web/Controllers/MoviesController.cs
@@ -16,7 +16,22 @@ namespace MovieApp.Web.Controllers
                 new Movie() { Title = "The Shawshank Redemption", Description =  "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.", Players = new string[] { "Tim Robbins", "Morgan Freeman", "Bob Gunton" }, ImageUrl = "The-Shawshank-Redemption.jpg"  },
                 new Movie() { Title = "The Dark Knight", Description = "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.", Director = "Christopher Nolan", Players = new string[] { "Christian Bale", "Heath Ledger", "Aaron Eckhart" }, ImageUrl = "The-Dark-Knight.jpg" },
             };
-            return View("Movies", filmListesi);
+
+            var turListesi = new List<Genre>()
+            {
+                new Genre() { Name = "Crime" },
+                new Genre() { Name = "Drama" },
+                new Genre() { Name = "Action" },
+                new Genre() { Name = "Thriller" }
+            };
+
+            var model = new MovieGenreViewModel()
+            {
+                Movies = filmListesi,
+                Genres = turListesi
+            };
+
+            return View("Movies", model);
         }
 
         // /movies/details
