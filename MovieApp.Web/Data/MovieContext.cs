906672e82a71bb9fca1de57376122c297b966b26
Adem Hatay <hatayadem5@gmail.com>
2024-01-31 04:01:21 +0300

    EfCore Context

---
+++ MovieApp.Web/Data/MovieContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApp.Web.Entity;
+
+namespace MovieApp.Web.Data
+{
+    public class MovieContext: DbContext
+    {
+
+        public MovieContext(DbContextOptions<MovieContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //    {
+    //        optionsBuilder.UseSqlite("Data Source=movies.db");
+    //    }
+    //}
+}
